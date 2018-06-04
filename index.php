285125c05b73aaa638686164884a52e4af9e099a
malvaceae <malvaceae@hotmail.com>
2018-06-05 03:00:00 +0900

    add resources

--- index.php
+++ index.php
@@ -2,3 +2,6 @@
 
 require_once "{$_SERVER['DOCUMENT_ROOT']}/libraries/idiorm.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/libraries/router.php";
+
+require_once "{$_SERVER['DOCUMENT_ROOT']}/resources/config.php";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/resources/routes.php";
