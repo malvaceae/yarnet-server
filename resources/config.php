49375784841701205b9b7392efffd0a01c30a953
malvaceae <malvaceae@hotmail.com>
2018-06-25 00:00:00 +0900

    add idiorm settings

--- resources/config.php
+++ resources/config.php
@@ -2,3 +2,8 @@
 
 // あらゆるリソースにアクセスを認めます。
 header('Access-Control-Allow-Origin: *');
+
+// DBライブラリ Idiorm の設定を行います。
+ORM::configure('mysql:host=localhost;dbname=yarnet;charset=utf8mb4');
+ORM::configure('username', 'dummy');
+ORM::configure('password', 'dummy');
