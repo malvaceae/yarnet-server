2b874281448c2f1389ddee2cc3d2a8ff4ef0c13e
malvaceae <malvaceae@hotmail.com>
2018-10-10 01:00:00 +0900

    add login route

--- resources/routes.php
+++ resources/routes.php
@@ -22,5 +22,29 @@ Router::post('/users', function() {
   echo json_encode(['id' => $users->id]);
 });
 
+Router::post('/login', function() {
+  $mail = $_POST['mail'] ?? NULL;
+  $pass = $_POST['pass'] ?? NULL;
+
+  $errors['mail'] = validate_mail($mail);
+  $errors['pass'] = validate_pass($pass);
+
+  if ($errors = array_filter($errors, function($e) { return $e; })) {
+    die(json_encode(['error' => $errors]));
+  }
+
+  $users = ORM::forTable('users')
+      ->selectMany('id', 'pass')
+      ->where('mail', $mail)
+      ->findOne();
+
+  if (!$users || !password_verify($pass, $users->pass)) {
+    header('HTTP/1.0 401 Unauthorized');
+    die('{"error":"401 Unauthorized"}');
+  }
+
+  echo json_encode(['id' => $users->id]);
+});
+
 header('HTTP/1.0 404 Not Found');
 die('{"error":"404 Not Found"}');
